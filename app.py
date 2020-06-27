965be5a035851d7ffddcc3cc98ff28c9c185dff5
Colm Doyle <collydoyle_88@hotmail.com>
2020-06-27 11:43:12 +0000

    added adding new users function, revised styling

--- app.py
+++ app.py
@@ -16,3 +16,21 @@ mongo = PyMongo(app)
 @app.route('/get_users')
 def get_users():
     return render_template("users.html", users=mongo.db.users.find())
+
+
+@app.route('/new_user')
+def new_user():
+    return render_template("new_user.html")
+
+
+@app.route('/add_user', methods=['POST'])
+def add_user():
+    users =  mongo.db.users
+    users.insert(request.form.to_dict())
+    return render_template("users.html", users=mongo.db.users.find())
+
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=(os.environ.get('PORT')),
+            debug=True)
