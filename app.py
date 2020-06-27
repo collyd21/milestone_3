0213ec7956e49a2799b54c0cded32b719b05bc4c
Colm Doyle <collydoyle_88@hotmail.com>
2020-06-27 12:11:09 +0000

    added editing permit function

--- app.py
+++ app.py
@@ -47,6 +47,49 @@ def add_permit():
     return render_template("permits.html", permits=mongo.db.permits.find().sort("date"))
 
 
+@app.route('/edit_permit/<permits_id>')
+def edit_permit(permits_id):
+    the_permits =  mongo.db.permits.find_one({"_id": ObjectId(permits_id)})
+    return render_template('edit_permit.html', permit=the_permits)
+
+
+@app.route('/update_permit/<permits_id>', methods=["POST"])
+def update_permit(permits_id):
+    permits = mongo.db.permits
+    permits.update( {'_id': ObjectId(permits_id)},
+    {
+        'date':request.form.get('date'),
+        'crew_size':request.form.get('crew_size'),
+        'location': request.form.get('location'),
+        'supervisor': request.form.get('supervisor'),
+        'request_by':request.form.get('request_by'),
+        'contractor':request.form.get('contractor'),
+        'sub_contractor':request.form.get('sub_contractor'),
+        'work_description':request.form.get('work_description'),
+        'system_name':request.form.get('system_name'),
+        'system_number':request.form.get('system_number'),
+        'equipment_tools':request.form.get('equipment_tools'),
+        'method':request.form.get('method'),
+        'risk':request.form.get('risk'),
+        'spa':request.form.get('spa'),
+        'excavation':request.form.get('excavation'),
+        'scanned':request.form.get('scanned'),
+        'shored':request.form.get('shored'),
+        'battered':request.form.get('battered'),
+        'skills_card':request.form.get('skills_card'),
+        'confined_space':request.form.get('confined_space'),
+        'atmosphere':request.form.get('atmosphere'),
+        'trained_persons':request.form.get('trained_persons'),
+        'emerg_o2':request.form.get('emerg_o2'),
+        'elec_works':request.form.get('elec_works'),
+        'locks':request.form.get('locks'),
+        'persons_locks':request.form.get('persons_locks'),
+        'energy_released':request.form.get('energy_released'),
+        'hazards':request.form.get('hazards')
+    })
+    return render_template("permits.html", permits=mongo.db.permits.find().sort("date"))
+
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
