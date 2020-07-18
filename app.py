16595a9bef673dbdb19a46d72a05792ff80fb604
Colm Doyle <collydoyle_88@hotmail.com>
2020-07-18 16:56:16 +0000

    updated app.py

--- app.py
+++ app.py
@@ -226,4 +226,4 @@ def delete_permit(permits_id):
 if __name__ == "__main__":
     app.run(host=os.getenv("IP", "0.0.0.0"),
             port=int(os.getenv("PORT", "5000")),
-            debug=True)
+            debug=False)
